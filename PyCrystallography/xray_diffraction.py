fixed gradient probs

PyCrystallography/xray_diffraction.py
@@ -10,11 +10,16 @@ except:
     
 import matplotlib.pyplot as plt
 
+# fig params
+# orgin (bottom left)
+x_origin = 0
+y_origin = -10
+canvas_size = 16
 
 # make lattice
-plt.figure('Xray-Diffraction')
+plt.figure('Xray-Diffraction',figsize=(5,5))
 primitive_cell_2d = primitive_cell_2d('square')
-lattice_points = make_lattice_2d(primitive_cell_2d,depth=15)
+lattice_points = make_lattice_2d(primitive_cell_2d,depth=canvas_size)
 
 
 #print(lattice_points)
@@ -22,13 +27,18 @@ lattice_points = make_lattice_2d(primitive_cell_2d,depth=15)
 # fire particles
 
 # angle fro surface norm (degrees)
-theta = 40
+theta = 45
 
 #cant be zero min = 1
-num_of_particles = 10
-
+num_of_particles = 3
 #cant be zero min = 1
-spread = 1
+# x distance incomving particles are distribued across
+spread = 3
+
+
+#print experiment summary init conds
+print('init conds')
+print(' - {} particles spread across {} in x at a angle of {} degrees\n'.format(num_of_particles,spread,theta))
 
 #compute angles and collisions
 
@@ -40,6 +50,15 @@ def braggs_law(n,lambd,theta):
 
     return d
 
+
+def check_deg(x1,x2,y1,y2):
+    """
+    checks a gradien matches an angle
+    """
+    deg = np.degrees(np.arctan((y2-y1)/(x2-x1)))
+    print(deg)
+
+
 def collisions(lattice_points,particle_num,theta,spread):
     """
     boolean colision indicator
@@ -49,43 +68,55 @@ def collisions(lattice_points,particle_num,theta,spread):
         x = lattice_point[0]
         y = lattice_point[1]
 
-        d_x = (particle_num+1/num_of_particles)*spread
-        c = d_x*np.tan(theta)
+        m = np.tan(theta)
 
-        y_calc = np.tan(theta)*x + c
+        d_x = (particle_num/num_of_particles)*spread
+        c = y_origin
+        #print(c)
+
+        
+        # calcualte x and y of particle traj given lattic coords
+        y_calc = m*(x + d_x) +c
+        x_calc = (y -c)/m + d_x
+       # print(x,x_calc)
+
 
         #print (c)
 
         #detection sensitivity
-        ds = 0.4
-        if (y_calc -ds< y < y_calc + ds):
-            print('collision')
-            print(y)
-            return y_calc
+        ds = 0.9
 
-    return None
+        if (x -ds < x_calc < x + ds) and (y - ds < y_calc< y + ds):
+            print('collision at ',x,x_calc,y,y_calc)
+            return x_calc, y_calc
+
+    return None,None
 
 # reflect paricles
 #plt.plot()
 #collisions(lattice_points,num_of_particles,theta,spread)
 
 theta = np.radians(theta)
-x_origin = -1
-y_origin = -10.5
 
-canvas_size = 16
+num_of_collisions = 0
 
 for i in range(0,num_of_particles):
 
-    x1 = x_origin + (i+1/num_of_particles)*spread
+    x1 = x_origin + (i/num_of_particles)*spread
     y1 =y_origin
 
-    if collisions(lattice_points,i,theta,spread):
-        y2 = collisions(lattice_points,i,theta,spread)    
-        d_x = (i+1/num_of_particles)*spread
-        c = d_x*np.tan(theta)   
-        x2 = (y2 - c)/np.tan(theta) #+ 
-        # plot reflected particle
+    x2c,y2c = collisions(lattice_points,i,theta,spread) 
+    if x2c != None:
+
+        x2,y2 = x2c,y2c
+        
+        num_of_collisions += 1
+
+        # d_x = (i/num_of_particles)*spread
+        # c = d_x*np.tan(theta)  
+
+        # x2 = (y2 )/np.tan(theta) + d_x
+        # # plot reflected particle
         x3 = x_origin + canvas_size
         m = (y2-y1)/(x2-x1)
 
@@ -93,13 +124,21 @@ for i in range(0,num_of_particles):
         c = 0
         y3 = -m*x3 + c
         plt.plot([x1,x2,x3],[y1,y2,y3]) 
-    else:
-        y2 = y_origin + canvas_size
-        x2 = (y2)/np.tan(theta) + (i+1/num_of_particles)*spread
         
-     #   plt.plot([x1,x2],[y1,y2])
+    else:
+        #working dont change
+        # if no collision plot til canvas edge
+        y2 = y_origin + canvas_size
+        c = y_origin
+        m = np.tan(theta)
 
-plt.xlim(x_origin,x_origin+canvas_size)
-plt.ylim(y_origin,y_origin+canvas_size)
+        x2 = (y2-c)/m + (i/num_of_particles)*spread
+        plt.plot([x1,x2],[y1,y2])
 
+plt.xlim(x_origin-0.5,x_origin+canvas_size-0.5)
+plt.ylim(y_origin-0.5,y_origin+canvas_size-0.5)
+
+print('final summary')
+print(' - {} lattice collision/s, {} lattice pass/es'.format(num_of_collisions,num_of_particles -num_of_collisions))
+plt.tight_layout()
 plt.show()