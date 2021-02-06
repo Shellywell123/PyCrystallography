8577deed05f2a48db4b1340b522e71dbaafd711e
Shellywell123 <benjamin@shellswell.com>
2021-02-06 18:59:13 +0000

    fixed rebound angles in xray

--- PyCrystallography/xray_diffraction.py
+++ PyCrystallography/xray_diffraction.py
@@ -1,5 +1,4 @@
-from PyCrystallography.lattice import make_lattice_2d
-from PyCrystallography.unit_cell import primitive_cell_2d
+
 
 import numpy as np
 import matplotlib
@@ -10,43 +9,29 @@ except:
     
 import matplotlib.pyplot as plt
 
-# fig params
-# orgin (bottom left)
-x_origin = 0
-y_origin = -10
-canvas_size = 16
+################################################################
+# functions
+################################################################
 
-# make lattice
-plt.figure('Xray-Diffraction',figsize=(5,5))
-primitive_cell_2d = primitive_cell_2d('square')
-lattice_points = make_lattice_2d(primitive_cell_2d,depth=canvas_size)
+def genrate_lattice_points(unit_cell_shape='square'):
+    """
+    generates lattice points on a figure
+    """
+    from PyCrystallography.lattice import make_lattice_2d
+    from PyCrystallography.unit_cell import primitive_cell_2d
 
+    plt.figure('Xray-Diffraction',figsize=(5,5))
+    primitive_cell_2d = primitive_cell_2d('square')
+    lattice_points = make_lattice_2d(primitive_cell_2d,depth=canvas_size)
 
-#print(lattice_points)
-
-# fire particles
-
-# angle fro surface norm (degrees)
-theta = 45
-
-#cant be zero min = 1
-num_of_particles = 3
-#cant be zero min = 1
-# x distance incomving particles are distribued across
-spread = 3
+    return lattice_points
 
 
-#print experiment summary init conds
-print('init conds')
-print(' - {} particles spread across {} in x at a angle of {} degrees\n'.format(num_of_particles,spread,theta))
-
-#compute angles and collisions
-
-def braggs_law(n,lambd,theta):
+def braggs_law(n,lambd,theta_rad):
     """
     return lattic distancing
     """
-    d = n*lambd/(2*np.sin(theta))
+    d = n*lambd/(2*np.sin(theta_rad))
 
     return d
 
@@ -59,86 +44,118 @@ def check_deg(x1,x2,y1,y2):
     print(deg)
 
 
-def collisions(lattice_points,particle_num,theta,spread):
+def collision_checker(lattice_points,m,c,d_x):
     """
-    boolean colision indicator
+    trajectory lattice colision checker
+    checks if a lattice point is within a range of y=mx+c
     """
+    # iterate through lattic points lowest x0 -> xmax, y0->ymax
     for lattice_point in lattice_points:
 
         x = lattice_point[0]
         y = lattice_point[1]
 
-        m = np.tan(theta)
-
-        d_x = (particle_num/num_of_particles)*spread
-        c = y_origin
-        #print(c)
-
-        
         # calcualte x and y of particle traj given lattic coords
-        y_calc = m*(x + d_x) +c
-        x_calc = (y -c)/m + d_x
-       # print(x,x_calc)
+        
+        x_calc = (y - c)/m + d_x
 
+        # fix y_calc!!!!!!!!
+        y_calc = m*(x - d_x) + c
 
-        #print (c)
+        #detection sensitivity (keep around 0.5 of lattice spacing)
+        ds = 0.2
 
-        #detection sensitivity
-        ds = 0.9
-
+        #see if trajectory in range of lattice point
+        # replace with radial checker soon
         if (x -ds < x_calc < x + ds) and (y - ds < y_calc< y + ds):
-            print('collision at ',x,x_calc,y,y_calc)
-            return x_calc, y_calc
+           # print('collision at ',x,x_calc,y,y_calc)
+            return (x_calc, y_calc)
 
-    return None,None
+    return None
 
-# reflect paricles
-#plt.plot()
-#collisions(lattice_points,num_of_particles,theta,spread)
 
-theta = np.radians(theta)
+def run_simulation(num_of_particles,theta_rad,spread):
+    """
+    runs xray diffraction simulations
+    """
+    lattice_points = genrate_lattice_points()
 
-num_of_collisions = 0
+    theta_rad = np.radians(theta)
 
-for i in range(0,num_of_particles):
+    num_of_collisions = 0
 
-    x1 = x_origin + (i/num_of_particles)*spread
-    y1 =y_origin
+    for i in range(0,num_of_particles):
 
-    x2c,y2c = collisions(lattice_points,i,theta,spread) 
-    if x2c != None:
-
-        x2,y2 = x2c,y2c
-        
-        num_of_collisions += 1
-
-        # d_x = (i/num_of_particles)*spread
-        # c = d_x*np.tan(theta)  
-
-        # x2 = (y2 )/np.tan(theta) + d_x
-        # # plot reflected particle
-        x3 = x_origin + canvas_size
-        m = (y2-y1)/(x2-x1)
-
-        #need to fgure out c
-        c = 0
-        y3 = -m*x3 + c
-        plt.plot([x1,x2,x3],[y1,y2,y3]) 
-        
-    else:
-        #working dont change
-        # if no collision plot til canvas edge
-        y2 = y_origin + canvas_size
+        m = np.tan(theta_rad)
         c = y_origin
-        m = np.tan(theta)
+        d_x = (i/num_of_particles)*spread
 
-        x2 = (y2-c)/m + (i/num_of_particles)*spread
-        plt.plot([x1,x2],[y1,y2])
+        x1 = x_origin + d_x
+        y1 = y_origin
 
-plt.xlim(x_origin-0.5,x_origin+canvas_size-0.5)
-plt.ylim(y_origin-0.5,y_origin+canvas_size-0.5)
+        collision = collision_checker(lattice_points,m,c,d_x)
 
-print('final summary')
-print(' - {} lattice collision/s, {} lattice pass/es'.format(num_of_collisions,num_of_particles -num_of_collisions))
-plt.tight_layout()
-plt.show()
+        if collision:
+            # if collision diffract particle trajectory
+            num_of_collisions += 1
+
+            x2,y2 = collision
+            
+            # set x lim to beat canvas edge
+            x3 = x2+(x2-x1)
+            y3 = y1
+
+            plt.plot([x1,x2,x3],[y1,y2,y3]) 
+            
+        else:
+            # if no collision plot til canvas edge
+            y2 = y_origin + canvas_size
+            x2 = (y2-c)/m + d_x
+
+            plt.plot([x1,x2],[y1,y2])
+
+    plt.xlim(x_origin-0.5,x_origin+canvas_size-0.5)
+    plt.ylim(y_origin-0.5,y_origin+canvas_size-0.5)
+
+    print('Simulation Outcome')
+    print(' - {} lattice collision/s, {} lattice pass/es'.format(num_of_collisions,num_of_particles -num_of_collisions))
+    plt.tight_layout()
+    plt.show()
+
+################################################################
+# setting params
+################################################################
+
+################################
+# fig params
+################################
+# fig params - orgin (bottom left)
+x_origin = 0
+y_origin = -10
+canvas_size = 16
+
+################################
+# simulation params
+################################
+
+# angle fro surface norm (degrees)
+theta = 60
+
+
+# number of particles being fired
+# - can not be zero, min = 1
+num_of_particles = 10
+
+# x distance incomving particles are distribued across
+# - can not be zero, min = 1
+spread = 5
+
+#print experiment summary init conds
+print('Initial Conditions')
+print(' - {} particles spread across {} (x units) at a angle of {} degrees\n'.format(num_of_particles,spread,theta))
+
+################################################################
+# execution
+################################################################
+
+run_simulation(num_of_particles,theta,spread)
